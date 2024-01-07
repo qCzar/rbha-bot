536551648618fa6c82d667981c0a6a714270e5b5
Alex G <comicidiot@gmail.com>
2024-01-07 16:44:33 -0600

    Update hardcoded database URL
    I hope this works

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = (_, argv) => {
       new DefinePlugin ({
         '__HOST__': JSON.stringify(
           (argv.mode === 'production')
-            ? 'https://www.s3bby.com/bored-bot'
+            ? 'https://discord.rochesterbored.com'
             : 'http://localhost:5000'
         )
       }),
