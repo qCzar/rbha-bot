4a008561c8ed4dba791150f04f56468aed843e1e
Alex G <comicidiot@gmail.com>
2024-01-10 22:17:47 -0600

    Update webpack.config.js - line 65 HTTPS -> HTTP
    DigitalOcean domain is not HTTPS

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = (_, argv) => {
       new DefinePlugin ({
         '__HOST__': JSON.stringify(
           (argv.mode === 'production')
-            ? 'https://discord.rochesterbored.com'
+            ? 'http://discord.rochesterbored.com'
             : 'http://localhost:5000'
         )
       }),
